bd4207c9b5a87b81ea450958e87d80f4e19a0dbe
Utkarsh_Chauhan <utkarshchauhan147@gmail.com>
2023-06-27 03:30:11 +0000

    day 1

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  // const Homepage({super.key});
+  int day = 49;
+  final hj = "gian";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome $day heer by $hj"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
---
+++ lib/pages/loginpage.dart
@@ -0,0 +1,55 @@
+import "package:flutter/material.dart";
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+      children: [
+        Image.asset("images/login.png", fit: BoxFit.cover),
+        SizedBox(
+          height: 22.0,
+        ),
+        Text(
+          "Welcome",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 22.0,
+        ),
+        Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "password",
+                  ),
+                ),
+                SizedBox(
+                  height: 22,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Hi Utkash");
+                  },
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                )
+              ],
+            ))
+      ],
+    ));
+  }
+}
