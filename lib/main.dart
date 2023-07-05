8568acc11e7c543c85939999d7819d0dc39b70c1
Utkarsh_Chauhan <utkarshchauhan147@gmail.com>
2023-07-05 12:47:44 +0000

    day 21

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         // home: Homepage(),
-        themeMode: ThemeMode.dark,
+        themeMode: ThemeMode.light,
         theme: MyTheme.lightTheme(context),
         darkTheme: MyTheme.darkTheme(context),
         debugShowCheckedModeBanner: false,
--- lib/utils/widgets/themes.dart
+++ lib/utils/widgets/themes.dart
@@ -31,7 +31,7 @@ class MyTheme {
           color: Colors.black,
           elevation: 0.0,
           iconTheme: IconThemeData(color: Colors.white),
-          titleTextStyle: TextStyle(color: Colors.black),
+          titleTextStyle: TextStyle(color: Colors.red),
         ),
       );
   static Color creamColor = Color(0xfff5f5f5);
